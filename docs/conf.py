fbbae9df78643cb4569ee81444ba8edf37f666ff
Matt Austin <matt.austin@thecut.net.au>
2014-07-08 13:48:49 +0800

    Fix import in sphinx conf.

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 
 import sys
 import os
-from .s3upload import __version__
+from s3upload import __version__
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
